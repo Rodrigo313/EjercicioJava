fae101874ce2cbc28df577ee19b6d1db46a09725
Rodrigo Pascual Montero <rodrigopascual1@hotmail.com>
2020-09-29 11:03:16 +0200

    [ADD] .gitignore | clase presentacion vacia

---
+++ src/com/rodri/presentacion/Presentacion.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.rodri.presentacion;
+
+/**
+ * @author rodri
+ *
+ */
+public class Presentacion {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Apéndice de método generado automáticamente
+
+	}
+
+}
