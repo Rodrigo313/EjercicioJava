ebb30d5673072b5e10d15be4a1e8cec719eda8d9
Rodrigo Pascual Montero <rodrigopascual1@hotmail.com>
2020-10-02 08:39:01 +0200

    [ADD] Ejercicio 1

---
+++ src/com/rodri/negocio/Negocio.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.rodri.negocio;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import com.rodri.datos.ConsultaDatos;
+
+/**
+ * @author rodri
+ *
+ */
+public class Negocio {
+	
+	public Integer numeroCoches(String nombre) throws IOException {
+		int numCoches = 0;
+		ConsultaDatos cd = new ConsultaDatos();
+		ArrayList <String> listaNombres = new ArrayList<String>(cd.consultaCoches(nombre));
+		for(int i = 0; i < listaNombres.size();i++) {
+			if(listaNombres.get(i).equals(nombre)) {
+				numCoches++;
+			}
+		}
+		return numCoches;	
+	}
+}
--- src/com/rodri/presentacion/Presentacion.java
+++ src/com/rodri/presentacion/Presentacion.java
@@ -3,6 +3,11 @@
  */
 package com.rodri.presentacion;
 
+import java.io.IOException;
+import java.util.Scanner;
+
+import com.rodri.negocio.Negocio;
+
 /**
  * @author rodri
  *
@@ -11,9 +16,16 @@ public class Presentacion {
 
 	/**
 	 * @param args
+	 * @throws IOException 
 	 */
-	public static void main(String[] args) {
-		// TODO Apéndice de método generado automáticamente
+	public static void main(String[] args) throws IOException {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Introduce un nombre");
+		
+		String nombre = sc.next();
+		
+		System.out.println("El número de coche(s) es: " + new Negocio().numeroCoches(nombre));
 
 	}
 
